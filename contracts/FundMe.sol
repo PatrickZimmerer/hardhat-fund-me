52f5a1d1fd729af019ed2686dee01a5e1a69bdf0
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-02-05 13:49:00 +0100

    Added Natspec comments + styleguide comments

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,28 +1,68 @@
 // SPDX-License-Identifier: MIT
+// Solidity Version
 pragma solidity ^0.8.8;
-
+// Imports
 import "./PriceConverter.sol";
+// Error Codes with double underscore plus name beforehand
+// to identify error source
+error FundMe__NotOwner();
 
-error NotOwner();
+// Interfaces, Libraries, Contracts
 
+// NATSPEC Commenting type
+/// @title A contract for crowd funding
+/// @author Patrick Zimmerer
+/// @notice This contract is to demo a sample funding contract
+/// @dev This implements price feeds as our library
 contract FundMe {
+    // Type Declarations
+
     using PriceConverter for uint256;
 
-    uint256 public constant MINIMUM_USD = 50 * 1e18;
-
-    address[] public funders;
+    // State Variables
 
     mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
 
     address public immutable i_owner;
-
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
     AggregatorV3Interface public priceFeed;
+
+    // Events / Modifiers
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
+            revert FundMe__NotOwner();
+        }
+        // require(msg.sender == i_owner, "Sender is not owner!");
+        _;
+    }
+
+    // Functions Order:
+    // constructor
+    // receive
+    // fallback
+    // external
+    // public
+    // internal
+    // private
+    // view / pure
 
     constructor(address priceFeedAddress) {
         i_owner = msg.sender;
         priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    /// @notice This functions funds this contract
+    /// @dev This puts the funder in the funder array and mapps the sent value to his address
     function fund() public payable {
         require(
             msg.value.getConversionRate(priceFeed) >= MINIMUM_USD,
@@ -32,6 +72,8 @@ contract FundMe {
         addressToAmountFunded[msg.sender] = msg.value;
     }
 
+    /// @notice This functions is used to withdraw the funds in this contract
+    /// @dev This resets the funders array
     function withdraw() public onlyOwner {
         for (uint256 i = 0; i < funders.length; i++) {
             address funder = funders[i];
@@ -43,21 +85,5 @@ contract FundMe {
             value: address(this).balance
         }("");
         require(callSuccess, "Call failed");
-    }
-
-    modifier onlyOwner() {
-        if (msg.sender != i_owner) {
-            revert NotOwner();
-        }
-        // require(msg.sender == i_owner, "Sender is not owner!");
-        _;
-    }
-
-    receive() external payable {
-        fund();
-    }
-
-    fallback() external payable {
-        fund();
     }
 }
