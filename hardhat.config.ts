28aade04133925b7908c27c93d771a5ab025dfad
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-02-05 13:08:09 +0100

    updated networks gas reporter and compiler version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,42 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+    defaultNetwork: "hardhat",
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }]
+    },
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5
+        }
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "ETH"
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0
+        }
+    }
 };
 
 export default config;
